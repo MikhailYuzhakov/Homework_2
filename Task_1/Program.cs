6f30a5cff3c1a276f362f58ced16ebdf0e2a78ae
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-04-17 22:24:18 +0300

    Вторая цифра трехзначного числа

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 999) {
+    Console.WriteLine("Необходимо ввести трехзначное число.");
+} else {
+    int digit_1 = number / 100; //отсекаем сотни
+    number = number - digit_1 * 100;
+
+    int digit_2 = number / 10; //отсекаем десятки
+    number = number - digit_2 * 10;
+
+    Console.Write("1-ая цифра числа: "); Console.WriteLine(digit_1);
+    Console.Write("2-ая цифра числа: "); Console.WriteLine(digit_2);
+    Console.Write("3-ья цифра числа: "); Console.WriteLine(number);
+}
